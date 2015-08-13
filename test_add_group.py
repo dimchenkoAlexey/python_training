c61d5a5558b15e1f055b096642f9505797c124dc
dimchenkoAlexey <dimchenko.alexey@gmail.com>
2015-08-13 23:09:36 +0300

    Revert "task 4"
    This reverts commit 0f83a009b6f9061cdfc0cbc843252d2620c1d343.

--- test_add_group.py
+++ test_add_group.py
@@ -1,23 +1,77 @@
 # -*- coding: utf-8 -*-
 from group import Group
-from application import Application
-import pytest
+from selenium.webdriver.firefox.webdriver import WebDriver
+import unittest
 
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy())
-    return fixture
+def is_alert_present(wd):
+    try:
+        wd.switch_to_alert().text
+        return True
+    except:
+        return False
 
 
-def test_add_new_group(app):
-    app.login(username="admin", password="secret")
-    app.create_new_group(Group(name="group3", header="header3", footer="footer3"))
-    app.logout()
+class test_add_group(unittest.TestCase):
+    def setUp(self):
+        self.wd = WebDriver()
+        self.wd.implicitly_wait(60)
 
+    def logout(self, wd):
+        # logout
+        wd.find_element_by_link_text("Logout").click()
 
-def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
-    app.create_new_group(Group(name="", header="", footer=""))
-    app.logout()
+    def create_new_group(self, wd, group):
+        # new group creation
+        wd.find_element_by_name("new").click()
+        # fill fields
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        wd.find_element_by_name("submit").click()
+
+    def open_groups_page(self, wd):
+        # open groups page
+        wd.find_element_by_link_text("groups").click()
+
+    def login(self, wd, username, password):
+        # login
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
+
+    def open_home_page(self, wd):
+        # open home page
+        wd.get("http://localhost/addressbook/")
+
+    def test_add_new_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_new_group(wd, Group(name="group3", header="header3", footer="footer3"))
+        self.logout(wd)
+
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_new_group(wd, Group(name="", header="", footer=""))
+        self.logout(wd)
+
+    def tearDown(self):
+        self.wd.quit()
+
+if __name__ == '__main__':
+    unittest.main()
