e0a1b88f774f3c4e3f5a6e2ba4b645b1981c014c
dimchenkoAlexey <dimchenko.alexey@gmail.com>
2015-08-14 00:24:31 +0300

    task 5

--- fixture/group.py
+++ fixture/group.py
@@ -26,3 +26,9 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         wd.find_element_by_name("submit").click()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("delete").click()
